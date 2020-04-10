a0317fb6fbb2958ee4952ccd64514707b2cfd9b8
Vivek Singh Bhadauria <viveksbhadauria007@gmail.com>
2020-04-11 02:52:16 +0530

    created basic singalling server

---
+++ src/main/java/com/webrtc/signalling/WebRTCApplication.java
@@ -0,0 +1,13 @@
+package com.webrtc.signalling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebRTCApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebRTCApplication.class, args);
+    }
+
+}
